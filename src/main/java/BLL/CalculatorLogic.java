1f39712a3695459c2071523dca13bf6bdb1a561f
danyazero <formulamgo2@gmail.com>
2023-03-28 19:35:39 +0300

    inline var

--- src/main/java/BLL/CalculatorLogic.java
+++ src/main/java/BLL/CalculatorLogic.java
@@ -7,22 +7,20 @@ public class CalculatorLogic implements ICalculator{
     public Result calculateIntegral(int intervals, int threads) {
         double a = 0;
         double b = Math.PI/3;
-        int n = intervals;
 
-        int nThreads = threads;
-        double delta = (b - a) / nThreads;
+        double delta = (b - a) / threads;
         totalResult = 0;
         finished = 0;
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < nThreads; i++) {
+        for (int i = 0; i < threads; i++) {
             IntegralCalculator calculator = new IntegralCalculator(a + i * delta,
                     a + i * delta + delta,
-                    n / nThreads, t -> Math.cos(4*t)*Math.cos(2*t), this);
+                    intervals / threads, t -> Math.cos(4*t)*Math.cos(2*t), this);
             new Thread(calculator).start();
         }
         try {
             synchronized (this) {
-                while (finished < nThreads) {
+                while (finished < threads) {
                     wait();
                 }
             }
